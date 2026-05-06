3280d4e0dc5b63e4e274b1cd4dec770b1952838d
Khalos Moscato <khalos.moscato@gmail.com>
2026-05-06 16:54:12 +0100

    fix: update GlobalUsing files

--- RecordShop.Tests/GlobalUsings.cs
+++ RecordShop.Tests/GlobalUsings.cs
@@ -1,4 +1,4 @@
-global using Moq;
-global using RecordShop.Api.Models;
-global using RecordShop.Api.Repositories;
+//global using Moq;
+//global using RecordShop.Api.Models;
+//global using RecordShop.Api.Repositories;
 global using Xunit;
